130a7307f9aedb5043661237ddf6c275a0fa0d78
ClaraSantosmf <clarasantosmf@gmail.com>
2023-01-30 13:41:13 -0300

    tirando o mundo de else do exercício do palindrome JS

--- Javascript/Palindrome_Number.js
+++ Javascript/Palindrome_Number.js
@@ -1,16 +1,13 @@
-// 9. Palindrome Number Isso é easy. Não me julguem, eu estava com umas tasks braba na hora. Prometo melhorar
+// 9. Palindrome Number é easy. Não me julguem, eu estava com umas tasks braba na hora. Prometo melhorar
 // https://leetcode.com/problems/palindrome-number/description/
 // Você pode rodar no termina com -> node Javascript/Palindrome_Number.js
 
 var isPalindrome = function (x) {
   if (x >= 0) {
-    let palavra = x.toString();
-    if (palavra.split("").reverse().join("") == palavra) {
+    let numeroPositivoStr = x.toString();
+    if (numeroPositivoStr.split("").reverse().join("") == numeroPositivoStr) {
       return true;
-    } else {
-      return false;
     }
-  } else {
-    return false;
   }
+  return false;
 };
